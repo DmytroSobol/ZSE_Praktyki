2c2b1834bf73455184d1e36224a95c9eee7d7368
soboldima <79514945+soboldima@users.noreply.github.com>
2021-02-26 16:54:48 +0100

    Changed Zadanie4

--- src/first_week/statements/Zadanie.java
+++ src/first_week/statements/Zadanie.java
@@ -86,6 +86,15 @@ public class Zadanie {
             }
         }
 
+        if(x == y && y == z){
+            System.out.println("3 liczby są takie same");
+        }
+        else{
+            if(x == y || y == z || x == z){
+                System.out.println("2 liczby są takie same");
+            }
+        }
+
 
         //Zadanie 5
         System.out.println("\n Zadanie 5 \n");
